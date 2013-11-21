c3147b766e7bdafaecb23cd313c7dce68558c406
bjameson <brent@jamesonnetworks.com>
2013-11-21 00:32:00 -0500

    Working on start, about to refactor it a lot

--- bin/start.js
+++ bin/start.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var logging = require('./logging.js');
 var fs = require('fs');
+var path = require('path');
 var settings = require('../settings.json');
 var async = require('async');
 //var index = fs.readFileSync('index.html');
@@ -10,29 +11,59 @@ var librarySearchDirectories = [];
 
 var NOT_FOUND = 'There is no view or library file which corresponds to the request';
 
-// Walk a directory structure for all files inside of the structure
-var walk = function(dir, done) {
-	var results = [];
-	fs.readdir(dir, function(err, list) {
-		if (err) return done(err);
-		var i = 0;
-		(function next() {
-			var file = list[i++];
-			if (!file) return done(null, results);
-			file = dir + '/' + file;
-			fs.stat(file, function(err, stat) {
-				if (stat && stat.isDirectory()) {
-					walk(file, function(err, res) {
-            		results = results.concat(res);
-            		next();
-          		});
-        	} 
-        	else {
-          		results.push(file);
-          		next();
-        	}});
-    	})();
-	});
+// // Walk a directory structure for all files inside of the structure
+// var walk = function(dir, done) {
+//   var results = [];
+//   fs.readdir(dir, function(err, list) {
+//     if (err) return done(err);
+//     var i = 0;
+//     (function next() {
+//       var file = list[i++];
+//       if (!file) return done(null, results);
+//       file = dir + '/' + file;
+//       fs.stat(file, function(err, stat) {
+//         if (stat && stat.isDirectory()) {
+//           walk(file, function(err, res) {
+//             results = results.concat(res);
+//             next();
+//           });
+//         } else {
+//           results.push(file);
+//           next();
+//         }
+//       });
+//     })();
+//   });
+// };
+
+var walkSync = function (start, callback) {
+  var stat = fs.statSync(start);
+
+  if (stat.isDirectory()) {
+    var filenames = fs.readdirSync(start);
+
+    var coll = filenames.reduce(function (acc, name) {
+      var abspath = path.join(start, name);
+
+      if (fs.statSync(abspath).isDirectory()) {
+        acc.dirs.push(name);
+      } else {
+        acc.names.push(name);
+      }
+
+      return acc;
+    }, {"names": [], "dirs": []});
+
+    callback(start, coll.dirs, coll.names);
+
+    coll.dirs.forEach(function (d) {
+      var abspath = path.join(start, d);
+      walkSync(abspath, callback);
+    });
+
+  } else {
+    throw new Error("path: " + start + " is not a directory");
+  }
 };
 
 var loadModules = function(moduleExtension, loadModuleCallback) {
@@ -55,60 +86,49 @@ var loadModules = function(moduleExtension, loadModuleCallback) {
 	}
 
 	var files;
-	var libraryFiles = [];
 
 	async.series([
 		function(callback) {
+			files = [];
 			// Export all of the possible routes based on the views present in the Nirodha project
 			// Get a list of all the thtml files in the root of the project
 			if(searchOptions.directoryIsRoot) {
-				files = fs.readdirSync(searchOptions.directories[0]).toString().split(',');
-				logging('Files from fs.readdirSync: ' + files, 7);
+				var dir = searchOptions.directories[0];
+				rootFiles = fs.readdirSync(dir);
+
+				logging('Root files: ' + rootFiles,7);
+				files.push({"fileNames": rootFiles.toString().split(','), "dir": "." });
+				//logging('Files from fs.readdirSync: ' + files, 7);
+
 				callback(null, files);
 			}
 			else {
-				var tempcb = function(err, result) {
-					libraryFiles.push(result);
+				for(var index in searchOptions.directories) {
+					var directory = searchOptions.directories[index];
+					walkSync(directory, function(dir, directories, fileNames) {
+						files.push({ "fileNames": name, "dir": dir});
+						//logging('Loading file ' + one + '/' + three, 7);
+					});
+
+					if(index == searchOptions.directories.length-1) {
+						callback(null, files);
+					}
 				}
-				for(var directory in searchOptions.directories) {
-					walk(directory, tempcb);
-				}
-				callback(null, libraryFiles);
 			}
 		},
 		function(callback) {
 			var fileListResult = [];
-			if(searchOptions.directoryIsRoot) {
-				for(var i = 0; i < files.length; i++) {
-					logging('Checking ' + files[i] + ' for proper extension (' + moduleExtension + '), Add it?: ' + (files[i].split('.')[files[i].split('.').length-1] === moduleExtension));
-					if(files[i].split('.')[files[i].split('.').length-1] === moduleExtension) {
-						// Add the files to the list of files
-						fileListResult.push(files[i].substring(0, files[i].length - moduleExtension.length) + moduleExtension);
-					}
-				}
-				console.log('The following files with type ' + moduleExtension + ' have been loaded:');
-				for(var i = 0; i < fileListResult.length; i++) {
-					console.log(fileListResult[i]);
+			for(var i = 0; i < files.length; i++) {
+				//logging('Checking ' + files[i] + ' for proper extension (' + moduleExtension + '), Add it?: ' + (files[i].split('.')[files[i].split('.').length-1] === moduleExtension));
+				logging('Files is ' + JSON.stringify(files));
+				if(files[i].fileNames.split('.')[files[i].fileNames.split('.').length-1] === moduleExtension) {
+					// Add the files to the list of files
+					fileListResult.push({});
 				}
 			}
-			else {
-				for(var k=0; k < libraryFiles.length; k++) {
-					for(var i = 0; i < libraryFiles[k][i].length; i++) {
-						if(files[i].split('.')[libraryFiles[k][i].split('.').length-1] === moduleExtension) {
-							// Add the files to the list of files
-							fileListResult.push(libraryFiles[k][i].substring(0, files[i].length - moduleExtension.length) + moduleExtension);
-						}
-					}
-				}
-				console.log('The following files with type ' + moduleExtension + ' have been loaded:');
-				if(fileListResult.length == 0) {
-					console.log('No files with ' + moduleExtension + ' extension have been found...');
-				}
-				else {
-					for(var i = 0; i < fileListResult.length; i++) {
-						console.log(fileListResult[i]);
-					}
-				}
+			console.log('The following files with type ' + moduleExtension + ' have been loaded:');
+			for(var i = 0; i < fileListResult.length; i++) {
+				console.log(fileListResult[i]);
 			}
 
 			callback(null, fileListResult)
@@ -119,7 +139,7 @@ var loadModules = function(moduleExtension, loadModuleCallback) {
 module.exports = function (args) {
 	// Set up search
 	rootSearchDirectories.push('.');
-	librarySearchDirectories.push('./libs');
+	librarySearchDirectories.push('./custom');
 	librarySearchDirectories.push(settings.path_to_nirodha + 'libs');
 
 	logging('Entering module loading...', 7);
@@ -139,14 +159,14 @@ module.exports = function (args) {
 			if(err) {
 				logging(err, 3);
 			}
-			logging('The results from loading: ' + JSON.stringify(results), 7);
+			//logging('The results from loading: ' + JSON.stringify(results), 7);
 			var htmlFiles = results[0][1]; 
 			var jsFiles = results[1][1];
 			var cssFiles = results[2][1];
 
-			logging('htmlFiles: ' + htmlFiles, 7);
-			logging('jsFiles: ' + jsFiles, 7);
-			logging('cssFiles: ' + cssFiles, 7);
+			// logging('htmlFiles: ' + htmlFiles, 7);
+			// logging('jsFiles: ' + jsFiles, 7);
+			// logging('cssFiles: ' + cssFiles, 7);
 
 			logging('Creating server ...');
 			http.createServer(function (req, res) {
@@ -159,22 +179,25 @@ module.exports = function (args) {
 					// Parse the request url
 					var URI = req.url.substring(1, req.url.length);
 					logging('Requested URI is: ' + URI, 7);
+					if(URI.indexOf('html') > 0) {
+						for(var i = 0; i < htmlFiles.length; i++) {
+							if(htmlFiles[i].name === URI) {
+								logging('A matching view for ' + req.url + ' has been found, reading and serving the page...');
 
-					if(htmlFiles.indexOf(URI) != -1) {
-						logging('A matching view for ' + req.url + ' has been found, reading and serving the page...');
+								// Get a handle to a VM object
+								var vm = require('./viewManager.js');
+								vm.init(URI);
 
-						// Get a handle to a VM object
-						var vm = require('./viewManager.js');
-						vm.init(URI);
+								// Set the headers to NEVER cache results
+								res.writeHead(200, {
+									'Content-Type': 'text/html',
+									'cache-control':'no-cache'
+								});
 
-						// Set the headers to NEVER cache results
-						res.writeHead(200, {
-							'Content-Type': 'text/html',
-							'cache-control':'no-cache'
-						});
-
-						// Call into the ViewManager and parse the pages
-						vm.parse(res);
+								// Call into the ViewManager and parse the pages
+								vm.parse(res);
+							}
+						}
 					}
 					// Look for a static library matching the request
 					else if(false) {
