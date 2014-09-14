180a2e60735d18221af4035f5407671337b52a15
Brent Jameson <brent@jamesonnetworks.com>
2014-09-14 17:03:08 -0400

    Fixing travis CI build

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@ cd bin
 ../node_modules/jshint/bin/jshint *.js 
 cd ..
 mocha -R spec -u tdd --recursive -R xunit test/ > test-reports.xml
-istanbul cover _mocha -- -u tdd -R spec
+istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -u tdd -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
